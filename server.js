3588f57c546fe297b19c706e0c30e6b1f87c142b
Abenezer <aabenezzer@gmail.com>
2024-10-30 09:32:54 +0300

    add global error handling

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const logger = require("./utils/logger");
 require("express-async-errors");
 
 const users = require("./routes/users");
-const tasks = require('./routes/tasks');
+const tasks = require("./routes/tasks");
 const error = require("./middlewares/error");
-
 
 //setup
 const app = express();
@@ -30,8 +30,18 @@ app.get("/", (req, res) => {
 
 // setup routes
 app.use("/api/users/", users);
-app.use('/api/tasks/', tasks);
+app.use("/api/tasks/", tasks);
 app.use(error);
+
+process.on("uncaughtException", (err) => {
+  logger.error(err.message, err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  logger.error(err.message, err);
+  process.exit(1);
+});
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => console.log(`Server Listning at port ${PORT}...`));
